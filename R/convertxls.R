3caedf5281fd6485d06f69932a8c1ba29425cc4e
espkhq <31943924+espkhq@users.noreply.github.com>
2017-11-09 17:28:59 +1300

    Add files via upload

--- R/convertxls.R
+++ R/convertxls.R
@@ -1,10 +1,12 @@
 #' Convert XLS to CSV
 
 #'
-#' This function converts a .xlsx or .xls file into .csv files to the chosen directory and merges the sheets together into a single data frame. This function is dependent on installing the gnumeric package via command line or through some other method. See the resource for how to install on Linux.
+#' This function converts a .xlsx or .xls file into .csv files to the chosen directory and merges the sheets together into a single data frame. 
+#'This function is dependent on installing the gnumeric package via command line or through some other method. See the resource for how to install on Linux.
 
 #' Credits: Chris Wallace
 #' Adapted from: https://cwcode.wordpress.com/2014/04/11/reading-xlsx-files-into-r-quickly/
+#' Resource: https://linuxconfig.org/converting-xlsx-excel-format-files-to-csv-on-linux
 
 #' @param filename the Excel file to be split into .csv files.
 #' @param out.dir the directory that the .csv files are to be written to. Default is current working directory.
